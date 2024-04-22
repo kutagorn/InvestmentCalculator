5a96dfffde820646d69064e8c845992cd62bf98a
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2024-04-22 11:39:44 +0100

    Added Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import investmentImg from "../assets/investment-calculator-logo.png"
+export default function Header(){
+    return(
+        <div id="header">
+            <img src={investmentImg} alt="Money Image" />
+            <h1>Investment Calculator</h1>
+        </div>
+
+    )
+
+}
